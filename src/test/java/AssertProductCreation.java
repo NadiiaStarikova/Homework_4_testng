546871f4b1ce37754beccd996b904cbcf43361b3
NadiiaStarikova <33597768+NadiiaStarikova@users.noreply.github.com>
2018-03-17 14:38:17 +0200

    Update AssertProductCreation.java

--- src/test/java/AssertProductCreation.java
+++ src/test/java/AssertProductCreation.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class AssertProductCreation {
+public class AssertProductCreation extends Annotations{
 
     @Test
     public void validateNewProductCreation() {
@@ -17,12 +17,12 @@ public class AssertProductCreation {
         String expectedQuantity = ProductCreation.createdProductQuantity;
         String expectedPrice = ProductCreation.createdProductPrice;
 
-        WebDriver driver = new ChromeDriver();
+        //WebDriver driver = new ChromeDriver();
         //Переход на главную страницу магазина -> Все товары
         driver.get("http://prestashop-automation.qatestlab.com.ua/");
         WebDriverWait wait = new WebDriverWait(driver, 10);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//*[text()='Все товары']/..")));
-        WebElement allGoodsButton = driver.findElement(By.xpath(".//*[text()='Все товары']/.."));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"content\"]/section/a")));
+        WebElement allGoodsButton = driver.findElement(By.xpath("//*[@id=\"content\"]/section/a"));
         allGoodsButton.click();
 
         //Проверка наличия созданного продукта на странице магазина
@@ -31,17 +31,17 @@ public class AssertProductCreation {
         //Переход на страницу продукта
         WebElement newGood = driver.findElement(By.linkText(expectedName));
         newGood.click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("h1")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"product-details\"]/div[1]/span")));
 
         //Проверки соответствия названия , количества и цены тем значениям, которые вводились при создании продукта
-        WebElement actualName = driver.findElement(By.className("h1"));
+        WebElement actualName = driver.findElement(By.xpath("//*[@id=\"main\"]/div[1]/div[2]/h1"));
         assertEquals("Product name is incorrect", expectedName, actualName);
         System.out.println("Expected product name: " + expectedName + ". Actual product name: "  + actualName + ".");
-        WebElement productQuantity = driver.findElement(By.className("product-quantities"));
-        String actualQuantity = productQuantity.getText().substring(11, 13);
+        WebElement productQuantity = driver.findElement(By.xpath("//*[@id=\"product-details\"]/div[1]/span"));
+        String actualQuantity = productQuantity.getText().substring(0, 2);
         assertEquals("Quantity of product is incorrect", expectedQuantity, actualQuantity);
         System.out.println("Expected product quantity: " + expectedQuantity + ". Actual product quantity: "  + actualQuantity + ".");
-        WebElement productPrice = driver.findElement(By.className("current-price"));
+        WebElement productPrice = driver.findElement(By.xpath("//*[@id=\"main\"]/div[1]/div[2]/div[1]/div[1]/div/span"));
         String actualPrice = productPrice.getText().substring(0, 5);
         assertEquals("Price of product is incorrect", expectedPrice, actualPrice);
         System.out.println("Expected product price: " + expectedPrice + ". Actual product price: "  + actualPrice + ".");
